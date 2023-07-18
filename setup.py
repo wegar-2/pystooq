3409e1c9a74a5bf4bfdd5796efeb96960614efab
wegar2 <awegrzyn17@gmail.com>
2023-07-18 21:51:18 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,39 +3,23 @@ import os
 import io
 import pathlib
 
-def read(*paths, **kwargs):
-    """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
-    '0.1.0'
-    >>> read("README.md")
-    ...
-    """
-
-    content = ""
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *paths),
-        encoding=kwargs.get("encoding", "utf8"),
-    ) as open_file:
-        content = open_file.read().strip()
-    return content
-
-
-def read_requirements(path):
-    return [
-        line.strip()
-        for line in read(path).split("\n")
-        if not line.startswith(('"', "#", "-", "git+"))
-    ]
-
-
 
 setup(
     name="pystooq",
-    version="1.0.0",
+    version="1.0.2",
     description="Package for fetching of data from Stooq.com",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type='text/markdown',
     author="Artur Wegrzyn",
     packages=find_packages(),
-    install_requires=read_requirements("requirements.txt")
+    install_requires=[
+        "coloredlogs>=15.0.1",
+        "humanfriendly>=10.0",
+        "numpy>=1.23.0",
+        "pandas>=2.0.3",
+        "python-dateutil>=2.8.2",
+        "pytz>=2023.3",
+        "six>=1.16.0",
+        "tzdata>=2023.3"
+    ]
 )
